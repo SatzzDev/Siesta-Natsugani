da4a11f3a21b5d06ed60c9cbc3a6abba5ca262a3
Satzz(BOT) <satganzdevs@gmail.com>
2024-05-13 19:51:06 +0700

    Add plugins/ttp.js

---
+++ plugins/ttp.js
@@ -0,0 +1,16 @@
+require('../config')
+const { Cmd } = require('../command.js');
+const { CS, jsonformat,reSize, ucapanWaktu, formatp, clockString, getBuffer, getCases, generateProfilePicture, sleep, fetchJson, runtime, pickRandom, getGroupAdmins, getRandom } = require("../lib/myfunc")
+const Scraper = require("@bochilteam/scraper")
+const {ftxt, ttle} = require('../lib/scrapes')
+
+Cmd({
+pattern: 'ttp',
+limit: true,
+desc: 'text To Picture',
+type: 'Converter'
+}, async (m, command, Satzz) => {
+const {q} = m
+if (!q) return m.reply('text?')
+Satzz.sendImageAsSticker(m.chat, `https://aemt.me/ttp?text=${q}`, m, { packname: global.packname, author: global.author })
+})
