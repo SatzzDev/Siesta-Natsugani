6531ba037bf9efa8e735e3adede8558485c5c10b
Satzz(BOT) <satganzdevs@gmail.com>
2024-05-13 19:51:03 +0700

    Add plugins/tovideo.js

---
+++ plugins/tovideo.js
@@ -0,0 +1,23 @@
+require('../config')
+const { Cmd } = require('../command.js');
+const axios = require('axios')
+const { CS, jsonformat,reSize, ucapanWaktu, formatp, clockString, getBuffer, getCases, generateProfilePicture, sleep, fetchJson, runtime, pickRandom, getGroupAdmins, getRandom } = require("../lib/myfunc")
+const Scraper = require("@bochilteam/scraper")
+const {ftxt, ttle} = require('../lib/scrapes')
+const { exec } = require("child_process");
+let { webp2mp4File } = require("../lib/uploader");
+
+Cmd({
+pattern: 'tovid|tovideo',
+limit: true,
+desc: 'sticker to image',
+type: 'Converter'
+}, async (m, command, Satzz) => {
+const quoted = m.quoted ? m.quoted : m
+const mime = (quoted.msg || quoted).mimetype || ''
+const qmsg = (quoted.msg || quoted)
+const {q} = m
+let medias = await Satzz.downloadAndSaveMediaMessage(qmsg);
+let ran = await webp2mp4File(medias);
+Satzz.sendMessage(m.chat, { video: await getBuffer(ran.result) }, { quoted: m });
+})
