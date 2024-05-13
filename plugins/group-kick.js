0b67d72d2168a337e277ff67f7626ac4f7970707
Satzz(BOT) <satganzdevs@gmail.com>
2024-05-13 19:50:00 +0700

    Add plugins/group-kick.js

---
+++ plugins/group-kick.js
@@ -0,0 +1,17 @@
+require('../config')
+const { Cmd } = require('../command.js');
+const { CS, jsonformat,reSize, ucapanWaktu, formatp, clockString, getBuffer, getCases, generateProfilePicture, sleep, fetchJson, runtime, pickRandom, getGroupAdmins, getRandom } = require("../lib/myfunc")
+const Scraper = require("@bochilteam/scraper")
+const {ftxt, ttle} = require('../lib/scrapes')
+
+Cmd({
+pattern: 'kick|kik',
+onlyGroup: true,
+onlyAdmins: true,
+desc: 'Kick Member',
+type: 'Group'
+}, async (m, command, Satzz) => {
+let {reply,q} = m
+let users = m.mentionedJid[0] ? m.mentionedJid : m.quoted ? [m.quoted.sender] : [q.replace(/[^0-9]/g, "") + "@s.whatsapp.net"];
+await Satzz.groupParticipantsUpdate(m.chat, users, "remove")
+})
